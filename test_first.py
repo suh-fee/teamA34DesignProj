025aee747b14ddde697147a63399a494e05473ef
Nassir <na2165@nyu.edu>
2021-04-08 19:40:53 -0400

    Making this test work

--- test_first.py
+++ test_first.py
@@ -1,6 +1,13 @@
 import pytest
-import source
+import source.models, source.__init__, source.main
+
+def f():
+    raise SystemExit(1)
 
 def bob():
     source.main.index()
     print("Index!")
+
+def test_func():
+    with pytest.raises(SystemExit):
+        bob()
