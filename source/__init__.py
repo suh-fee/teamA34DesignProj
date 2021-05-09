b925dff1462f28e4a35f94b75604a77d5987f625
ailyntyagi <ailyntyagim@gmail.com>
2021-05-08 22:22:51 -0400

    Fixed paths to import local libraries.

--- source/__init__.py
+++ source/__init__.py
@@ -20,18 +20,18 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    from models import User
+    from source.models import User
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
     # blueprint for auth routes
-    from auth import auth as auth_blueprint
+    from source.auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth routes
-    from main import main as main_blueprint
+    from source.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
