3e6a4da957f4fc9023adb6e4d98f4469bb2e5914
ailyntyagi <ailyntyagim@gmail.com>
2021-05-08 22:23:24 -0400

    Add functionality to allow a user to unlink a site.

--- source/main.py
+++ source/main.py
@@ -4,14 +4,15 @@ This is the file containining all of the non-auth routes for Flare app.
 
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
-from __init__ import db, create_app
-from models import User, Site
+from source.__init__ import db, create_app
+from source.models import User, Site
 
 import tweepy
 
 main = Blueprint('main', __name__)
 
 testaccounts = ["@nimcanttweet", "@twitter"]
+
 
 @main.route('/')
 def index():
@@ -70,6 +71,37 @@ def add_site_post():
     return redirect(url_for('main.profile'))
 
 
+@main.route('/remove_site')
+@login_required
+def remove_site():
+    """
+    The purpose of this function is to render the site that allows users to
+    remove social networking site associated with their account.
+    """
+    return render_template('remove_site.html', sns=current_user.sns)
+
+
+@main.route('/remove_site', methods=['POST'])
+@login_required
+def remove_site_post():
+    """
+    The purpose of this function is to obtain the information the user submits
+    when removing new social networking site to their profile. This
+    information is sent through an HTML POST request and is captured and
+    removed from the database.
+    """
+    handle = request.form.get('snsuser')
+
+    for i, s in enumerate(current_user.sns):
+        if s.handle == handle:
+            current_user.sns.pop(i)
+            db.session.delete(s)
+
+    db.session.commit()
+
+    return redirect(url_for('main.profile'))
+
+
 @main.route('/profile', methods=['POST'])
 @login_required
 def follow_test():
@@ -91,12 +123,10 @@ def follow_test():
         try:
             api.create_friendship(i)
         except:
-            print("couldn't follow account //")
+            print("Couldn't follow account. //")
 
     return render_template('profile.html', user=current_user,
                            sns=current_user.sns)
-
-
 
 
 app = create_app()
